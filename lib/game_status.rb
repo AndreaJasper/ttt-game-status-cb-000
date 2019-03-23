4b2254f93f9a23ac2feaefa1c6a1899746f71e77
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-03-23 23:37:43 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,12 +17,29 @@ WIN_COMBINATIONS = [
 
 #defines if there is a winning combination, draw, or if the board is empty
 def won?(board)
-  empty_board = board.all? {|space| space == " "}
-  draw = board.all? {|token| token == "X" || token == "O"}
-  WIN_COMBINATIONS.any? do |win_combo|
-    if win_combo.select? {|index| board[index]}
-      return win_combo
-    else
+empty_board = board.all? {|space| space == " "}
+draw = board.all? {|token| token == "X" || token == "O"}
+WIN_COMBINATIONS.each do |win_combo|
+  if win_combo.all? { |index| board[index] =="X" } || win_combo.all? { |index| board[index] =="O" }
+     return win_combo
+  else empty_board || draw
       return false
     end
+  end
+end
+
+#determines if the board is full
+def full?(board)
+  board.all? { |space| space == "X" || space == "O"}
+end
+
+#determines if there is a draw/nobody win
+def draw?(board)
+  if won?(board)
+    false
+  elsif !won?(board) && full?(board)
+    true
+  else
+    false
+  end
 end
