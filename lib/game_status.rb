434f9b6d1bb5e057acfc82602dfeb4d27ac6e866
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-03-24 00:44:39 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,7 +21,7 @@ empty_board = board.all? {|space| space == " "}
 draw = board.all? {|token| token == "X" || token == "O"}
 WIN_COMBINATIONS.all? do |win_combo|
   if win_combo.all? {|index| board[index.to_i] =="X" } || win_combo.all? {|index| board[index] =="O" }
-     return win_combo[index.select?]
+     return win_combo.select?[index]
   else empty_board || draw
       return false
     end
